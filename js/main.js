50dbc03a05f73b656f8f08d396f5f97a9a976272
Shruti <shruti.sundar0405@gmail.com>
2024-02-20 23:26:05 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -35,9 +35,48 @@ const blurHeader = () => {
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
-
+const scrollUp =() =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll'):scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('selection[id]')
+    const scrollActive = () =>{
+        const scrollDown = window.scrollY
 
+        sections.forEach(current =>{
+            const selectionHeight = current.offsetHeight,
+                  sectionTop = current.offsetTop - 58,
+                  sectionId = current.getAttribute('id'),
+                  sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+            if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+                sectionsClass.classList.add('active-link')
+            }else{
+                sectionsClass.classList.remove('action-link')
+            }
+        })
+    }
+    window.addEventListener('scroll',scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '40px',
+//     opacity: 1,
+//     scale: 1.1,
+//     duration: 2500,
+//     delay: 300,
+// })
+
+// sr.reveal(`.home__data, .about__img, .about__data, .visit__data`)
+
+// sr.reveal(`.home__image`, {rotate: {z:-15}})
+// sr.reveal(`.home__bread, .about__bread`, {rotate: {z:15}})
+// sr.reveal(`.home__footer`, {scale: 1, origin: 'bottom'})
+
+// sr.reveal(`.new__card:nth-child(1) img`, {rotate: {z:-30}, distance: 0})
+// sr.reveal(`.new__card:nth-child(2) img`, {rotate: {z:15}, distance: 0, delay:600})
+// sr.reveal(`.new__card:nth-child(3) img`, {rotate: {z:-30, distance: 0, delay: 900})
+
+// sr.reveal(`.favorite__card img`, {interval:100, rotate: {z:15, distance: 0})
